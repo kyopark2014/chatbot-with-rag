00eb1d77873a91abccc5ae501a6c9f2824e5facd
ksdyb.park <ksdyb@amazon.com>
2024-02-03 10:36:53 +0900

    update

---
+++ cdk-gemini-rag/test/cdk-gemini-rag.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGeminiRag from '../lib/cdk-gemini-rag-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-gemini-rag-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGeminiRag.CdkGeminiRagStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
